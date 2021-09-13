446bfd1f2cf0557bfd4d941bf144679acb3d84c1
Luís <lhsda.snf20@uea.edu.br>
2021-09-13 17:50:09 -0400

    Prints-Inputs-Repetições

---
+++ Projeto de Programas/exercicio-1-aula-12-09-21.java
@@ -0,0 +1,22 @@
+import javax.swing.JOptionPane;
+
+public class Principal{
+	public static void main(String[] args) {
+		
+		String v1 = JOptionPane.showInputDialog("Qual é o valor 1?");
+		int i = Interger.parseInt(v1);
+		String v2 = JOptionPane.showInputDialog("Qual é o valor 2");
+		int n = Interger.parseInt(v2);
+		
+		int resultadoSoma;
+		resultadoSoma = i + n;
+		int resultadoSubtracao;
+		resultadoSubtracao = i - n;
+		int resultadoMultiplicacao;
+		resultadoMultiplicacao = i * n;
+		float resultadoDivisao;
+		resultadoDivisao = i/n;
+	
+		JOptionPane.showMessageDialog(null, resultadoSoma + "" + resultadoSubtracao + "" + resultadoMultiplicacao + "" + String.format("%.2f", resultadoDivisao));
+	} 
+}
