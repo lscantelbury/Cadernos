0415d96a1bf8e86bf38526e3a2d88f1dc05915dc
Luís <lhsda.snf20@uea.edu.br>
2021-10-04 14:48:54 -0400

    Commit

---
+++ Organizador_de_vagoes.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#define LOG(x) cout << x << endl
+
+using namespace std;
+
+int main(){
+		int casosDeTeste, tamanhoDoTrem, ordemCorrente[], swap;
+		cout << "";
+		cin >> casosDeTeste;
+		LOG(casosDeTeste);
+		return 0;
+}
