a3dec7e7a9746ae8405e33c5e6bbfca0af15500d
Luis <lhsda.snf20@uea.edu.br>
2021-11-16 17:27:05 -0400

    Produtor Consumidor com timer

--- SO-I/threads-semaforos.py
+++ SO-I/threads-semaforos.py
@@ -1,5 +1,6 @@
 from threading import Thread
 from threading import Semaphore
+import os
 import time
 import random
 
@@ -16,7 +17,6 @@ class Produtor(Thread):
 
     def run(self):
         intAleatorio = None
-        if len(lista) == 10: return
         while True:
             podeProduzir.acquire() # Produziu 1, subtrai 1 dos que ainda podem produzir
             listaDiponivel.acquire() # Lista já está sendo utilizada, reservá-la para esse processo
@@ -24,9 +24,10 @@ class Produtor(Thread):
             intAleatorio = random.randint(0, 10)
             self.lista.append(intAleatorio)
             print("Thread Produtor " + str(self.num) + " adicionou " + str(intAleatorio) + ':' + str(self.lista))
+            time.sleep(0.5)
 
-            podeConsumir.release() # Já tem algo para ser consumido
             listaDiponivel.release() # Terminei o processo, lista está liberada
+            podeConsumir.release() # Já tem algo para ser consumido
 class Consumidor(Thread):
     def __init__(self, lista, num):
         Thread.__init__(self)
@@ -35,22 +36,45 @@ class Consumidor(Thread):
     
     def run(self):
         intAleatorio = None
-        if len(lista) == 0: return
         while True:
+            if len(lista) == 10:break
             podeConsumir.acquire() # Consumi um dado dos que podiam ser consumidos
             listaDiponivel.acquire() # Estou utilizando a lista, reservá-la pra mim
 
             intAleatorio = self.lista.pop(0)
             print("Thread Consumidor " + str(self.num) + " retirou " + str(intAleatorio) + ':' + str(self.lista))
+            time.sleep(0.5)
 
-            podeProduzir.release() # Mais um item pode ser produzido
             listaDiponivel.release() # Terminei, pode utilizar a lista
+            podeProduzir.release() # Mais um item pode ser produzido
 
-produtor1 = Produtor(lista, 1)
-produtor1.start()
-consumidor1 = Consumidor(lista, 1)
-consumidor1.start()
-produtor2 = Produtor(lista, 2)
-produtor2.start()
-consumidor2 = Consumidor(lista, 2)
-consumidor2.start()
+class Relogio(Thread):
+    def __init__(self, secs):
+        Thread.__init__(self)
+        self.secs = secs
+        self.contador = 0
+
+    def run(self):
+        while self.contador != self.secs:
+            time.sleep(1)
+            self.contador += 1
+        os._exit(0)
+def main():
+    timer = Relogio(20) # O programa vai executar por 20 segundos
+    timer.start()
+
+    produtor1 = Produtor(lista, 1)
+    produtor1.start()
+
+    consumidor1 = Consumidor(lista, 1)
+    consumidor1.start()
+
+    produtor2 = Produtor(lista, 2)
+    produtor2.start()
+
+    consumidor2 = Consumidor(lista, 2)
+    consumidor2.start()
+
+    exit()
+
+main()
